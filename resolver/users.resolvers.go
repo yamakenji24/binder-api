9195bfafd8be07f162d0b8c94a691bf0700ca9a2
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-06-05 22:36:18 +0900

    delete login mutation

--- resolver/users.resolvers.go
+++ resolver/users.resolvers.go
@@ -5,27 +5,12 @@ package resolver
 
 import (
 	"context"
-	"fmt"
 	"strconv"
-
-	"golang.org/x/crypto/bcrypt"
 
 	"github.com/yamakenji24/binder-api/graph/generated"
 	"github.com/yamakenji24/binder-api/graph/model"
 	"github.com/yamakenji24/binder-api/repository"
 )
-
-func (r *mutationResolver) Login(ctx context.Context, input model.LoginInput) (string, error) {
-	user, err := repository.GetUserByName(input.Username)
-	if err != nil {
-		return "", err
-	}
-	if !compareHashedPassword(user.Password, input.Password) {
-		return "", fmt.Errorf("Invalid username and password combination!")
-	}
-	// TODO: create jwt token
-	return "jwt token will be returned", nil
-}
 
 func (r *queryResolver) User(ctx context.Context, username string) (*model.GraphUser, error) {
 	user, err := repository.GetUserByName(username)
@@ -40,19 +25,7 @@ func (r *queryResolver) User(ctx context.Context, username string) (*model.Graph
 	}, nil
 }
 
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
-
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-func compareHashedPassword(hash string, pass string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
-	if err == nil {
-		return true
-	}
-	return false
-}
