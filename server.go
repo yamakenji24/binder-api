740802867761c9857aef0d6a672f1277ac9339c9
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-06-05 22:37:09 +0900

    add login endpoint

--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/yamakenji24/binder-api/controllers"
+	"github.com/yamakenji24/binder-api/crypt"
 	"github.com/yamakenji24/binder-api/graph/generated"
 	"github.com/yamakenji24/binder-api/resolver"
 )
@@ -29,16 +34,40 @@ func GinContextToContextMiddleware() gin.HandlerFunc {
 	}
 }
 
+func CheckJWTHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader("authorization")
+		if len(header) == 0 {
+			c.Abort()
+		}
+
+		token, err := jwt.Parse(header, func(token *jwt.Token) (interface{}, error) {
+			return crypt.NewPublicKey(), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": http.StatusText(http.StatusUnauthorized),
+			})
+			c.Abort()
+		}
+	}
+}
+
 func main() {
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		AllowCredentials: false,
-		AllowAllOrigins:  true,
-	}))
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowHeaders = []string{"Authorization", "Content-Type"}
+
+	router.Use(cors.New(config))
+
+	router.POST("/login", func(c *gin.Context) {
+		controllers.Login(c)
+	})
 
 	router.Use(GinContextToContextMiddleware())
+	router.Use(CheckJWTHandler())
 
 	router.POST("/query", graphqlHandler())
 	router.GET("/", playgroundHandler())
