28c9f54cb4b218aec2bc6ce309d7a6edfdc6b6da
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-06-11 02:13:21 +0900

    decode base64 and make pdf filePath

--- resolver/documents.resolvers.go
+++ resolver/documents.resolvers.go
@@ -6,21 +6,29 @@ package resolver
 import (
 	"context"
 	"encoding/base64"
-	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/yamakenji24/binder-api/graph/generated"
 	"github.com/yamakenji24/binder-api/graph/model"
+	"github.com/yamakenji24/binder-api/repository"
 )
 
 func (r *mutationResolver) CreateDocument(ctx context.Context, input model.DocumentInput) (*model.GraphDocument, error) {
+	userID, _ := ctx.Value("userId").(int)
+	filepath, err := decodeFile(userID, input.Title, input.File)
+	if err != nil {
+		return &model.GraphDocument{}, err
+	}
 
-	decData := decodeFile(1, input.Title, input.File)
-	fmt.Println(decData)
+	// DB 登録とか？
+	doc, err := repository.CreateNewDocument(userID, input.Title, input.Description, filepath)
 
 	return &model.GraphDocument{
-		ID:          "1",
-		Title:       input.Title,
-		Description: input.Description,
+		ID:          strconv.FormatUint(uint64(doc.ID), 10),
+		Title:       doc.Title,
+		Description: doc.Description,
 		File:        input.File,
 	}, nil
 }
@@ -30,9 +38,20 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 
 type mutationResolver struct{ *Resolver }
 
-func decodeFile(id int, title string, file string) []byte {
+func decodeFile(id int, title string, inputfile string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(inputfile)
+	if err != nil {
+		return "", err
+	}
+	dir, err := os.Getwd()
+	fp := dir + "/document/" + strconv.Itoa(id) + "_" + strconv.FormatInt(time.Now().Unix(), 10) + ".pdf"
 
-	data, _ := base64.StdEncoding.DecodeString(file)
+	file, err := os.Create(fp)
+	defer file.Close()
+	file.Write(data)
+	if err != nil {
+		return "", err
+	}
 
-	return data
+	return fp, nil
 }
